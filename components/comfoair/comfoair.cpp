2de465d4f9fcace4d87ddee693fca97081824716
kayware <kay@kayone.pro>
2025-09-16 16:41:27 +0200

    Fix formatting in borrowed code

--- components/comfoair/comfoair.cpp
+++ components/comfoair/comfoair.cpp
@@ -15,7 +15,7 @@ climate::ClimateTraits ComfoAirComponent::traits() {
     auto traits = climate::ClimateTraits();
     traits.set_supports_current_temperature(true);
     traits.set_supported_modes({
-      climate::CLIMATE_MODE_FAN_ONLY
+        climate::CLIMATE_MODE_FAN_ONLY
     });
     traits.set_supports_two_point_target_temperature(false);
     traits.set_supported_presets({
@@ -26,11 +26,11 @@ climate::ClimateTraits ComfoAirComponent::traits() {
     traits.set_visual_max_temperature(29);
     traits.set_visual_temperature_step(1);
     traits.set_supported_fan_modes({
-      climate::CLIMATE_FAN_AUTO,
-      climate::CLIMATE_FAN_LOW,
-      climate::CLIMATE_FAN_MEDIUM,
-      climate::CLIMATE_FAN_HIGH,
-      climate::CLIMATE_FAN_OFF,
+        climate::CLIMATE_FAN_AUTO,
+        climate::CLIMATE_FAN_LOW,
+        climate::CLIMATE_FAN_MEDIUM,
+        climate::CLIMATE_FAN_HIGH,
+        climate::CLIMATE_FAN_OFF,
     });
     return traits;
 }
